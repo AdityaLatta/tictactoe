0a4614b3d9a38c2cc8024c28bbc9ff1906f67f0a
Aditya <adityalatta78@gmail.com>
2023-09-11 16:43:38 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,112 @@
-import Image from 'next/image'
+"use client"
 import styles from './page.module.css'
+import React, { useState } from 'react'
+
+
 
 export default function Home() {
+  const arr = ["", "", "", "", "", "", "", "", ""];  
+  let [chkarr, setchkarr] = useState(["", "", "", "", "", "", "", "", ""])
+  const [value, setvalue] = useState(arr);
+  const [count, setcount] = useState(0);
+  const [lock, setlock] = useState(false)
+  const [winner, setwinner] = useState("")
+
+  const handleClick = (e, i) => {
+
+    if(lock){
+      return;
+    }
+
+    if (count % 2 === 0) {
+      if (value[i]) {
+        return;
+      }
+
+      
+      let newarr = [...value];
+      newarr[i] = "X";   
+      setvalue(newarr);
+      chkarr[i] = "X"
+      setcount(count + 1);
+      chkwin();
+    }
+    else {
+      if (value[i]) {
+        return;
+      }
+      
+      let newarr = [...value];
+      newarr[i] = "O";
+      setvalue(newarr);
+      chkarr[i] = "O"
+      setcount(count + 1);
+      chkwin();
+    }
+
+    // console.log(chkarr)
+  }
+
+  const handleReset = () => {
+    setvalue(["", "", "", "", "", "", "", "", ""])
+    setchkarr(["", "", "", "", "", "", "", "", ""])
+    setlock(false);
+    setcount(0)
+    setwinner("")
+    console.log('working')
+  }
+
+  const chkwin = () => {
+    const list = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    // console.log(value)
+    
+    for (let i in list) {
+      const [a, b, c] = list[i]
+      
+
+      if(chkarr[a] && chkarr[a] === chkarr[b] && chkarr[a] === chkarr[c]){
+        console.log(chkarr[a])
+        setlock(true)
+        setwinner(chkarr[a])
+      }
+      }
+
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <div className={styles.header}>Tic Tak Toe Using <span>Next.Js</span></div>
+      {winner && <div className={styles.header}>Winner is : {winner}</div>}
+      
+      <div className={styles.boxes}>
+        <div className={styles.board}>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 0))}>{value[0]}</div>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 1))}>{value[1]}</div>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 2))}>{value[2]}</div>
+        </div>
+        <div className={styles.board}>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 3))}>{value[3]}</div>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 4))}>{value[4]}</div>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 5))}>{value[5]}</div>
+        </div>
+        <div className={styles.board}>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 6))}>{value[6]}</div>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 7))}>{value[7]}</div>
+          <div className={styles.box} onClick={(e) => (handleClick(e, 8))}>{value[8]}</div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <button className={styles.btn} onClick={handleReset}>Reset</button>
+    </div>
   )
 }
